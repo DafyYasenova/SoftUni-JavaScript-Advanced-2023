917eabc79ac0f7b5aa174009aea087db552b84e4
Dafy Yasenova <130154885+DafyYasenova@users.noreply.github.com>
2023-05-09 17:49:45 +0300

    Delete 4. DOM Intoduction/Lab - DOM Introduction directory

--- 4. DOM Intoduction/Lab - DOM Introduction/01.Edit Element/EditElement.js
+++
@@ -1,7 +0,0 @@
-function editElement(reference, match, replacer) {
-    
-    let content = reference.textContent;
-    let pattern = new RegExp(match, 'g');
-    let result = content.replace(pattern, replacer);
-    reference.textContent = result;
-}
