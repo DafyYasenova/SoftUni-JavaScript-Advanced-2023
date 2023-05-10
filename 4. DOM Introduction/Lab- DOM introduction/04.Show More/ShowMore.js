b1352f74002c6e98486f4850759ff0fd2d61cdf4
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-05-10 14:50:38 +0300

    Add showMore.js

---
+++ 4. DOM Introduction/Lab- DOM introduction/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    let showMore = document.getElementById('more')
+     showMore.style.display = 'none';
+
+    let showMoreText = document.getElementById('text');
+     showMoreText.style.display = 'inline';
+       
+}
