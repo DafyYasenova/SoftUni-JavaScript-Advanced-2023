6069fd4e0964041085cd4792bf34ca8e8491765d
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-05-14 17:12:35 +0300

    Add objectFactory problem

---
+++ 3. Advanced Objects and Composition/Lab- Advanced Objects and Composition/04-objectFactory.js
@@ -0,0 +1,66 @@
+function factory(library, orders){
+    let products = [];
+
+    for(const order of orders){
+        let currentProduct = order.template;
+
+        for(const part of order.parts){
+            currentProduct[part] = library[part];
+        }
+        products.push(currentProduct);
+    }
+    return products;
+}
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+        template: { name: 'ACME Printer'},
+        parts: ['print']      
+      },
+      {
+        template: { name: 'Initech Scanner'},
+        parts: ['scan']      
+      },
+      {
+        template: { name: 'ComTron Copier'},
+        parts: ['scan', 'print']      
+      },
+      {
+        template: { name: 'BoomBox Stereo'},
+        parts: ['play']      
+      }
+    ];
+    const products = factory(library, orders);
+    console.log(products);
+
+    /* [
+  {
+    name: 'ACME Printer',
+    print: [Function: print]
+  },
+  {
+    name: 'Initech Scanner',
+    scan: [Function: scan]
+  },
+  {
+    name: 'ComTron Copier',
+    scan: [Function: scan],
+    print: [Function: print]
+  },
+  {
+    name: 'BoomBox Stereo',
+        play: [Function: play]
+  }
+]
+
+ */
